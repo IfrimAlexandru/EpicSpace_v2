43f0ad084c773a6d1eadf231e61071f5361d6253
IfrimAlexandru <ifrimalexandru27@gmail.com>
2024-11-03 14:53:01 +0100

    aggiornati environments

--- FRONTEND/src/environments/environment.development.ts
+++ FRONTEND/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8888/',
+    apiUrl: 'https://epicspacev2-production.up.railway.app/',
     stripe: 'pk_test_51PUd3wK77lkHwPqgQivkj8Tz04ysibhDomLvRJXh5W4LGrzSmvi5tLP7eND53UO3WISiEgwTQvRi38PXXrJNiGVd00Iw2aeG1L',
     serverUrl: '/api',
     clientIdGoogle: '722812885951-dvlljpp34qhkm2mgd0vifbtncgh4b7sk.apps.googleusercontent.com'
--- FRONTEND/src/environments/environment.ts
+++ FRONTEND/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8888/'
+    apiUrl: 'https://epicspacev2-production.up.railway.app/'
     
 };
